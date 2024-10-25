eb2a2956abf3ac1d9b6c67a225e4d9f3bbed6028
EddAngulo <edangulo@uninorte.edu.co>
2024-10-25 18:49:21 -0500

    Feat: Client now can receive and send messages simultaneously

--- src/server.py
+++ src/server.py
@@ -1,53 +1,76 @@
 import socket
-from _thread import *
-from typing import List, NoReturn
+import threading
+from config import CONFIG_PARAMS
+from typing import List
 
-IP_ADDRESS = '172.17.9.62'
-PORT = 8081
-MAX_CLIENTS = 100
+# Configuration Parameters
+IP_ADDRESS = CONFIG_PARAMS['SERVER_IP_ADDRESS']
+PORT = CONFIG_PARAMS['SERVER_PORT']
+MAX_CLIENTS = CONFIG_PARAMS['SERVER_MAX_CLIENTS']
 LIST_OF_CLIENTS: List["socket.socket"] = []
 
-SERVER_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# Remove Client from List of Clients
+def remove_client(client_socket: "socket.socket") -> None:
+    if client_socket in LIST_OF_CLIENTS:
+        LIST_OF_CLIENTS.remove(client_socket)
 
-def client_thread(client_socket: "socket.socket", client_address: "socket._RetAddress") -> NoReturn:
-    client_socket.send(b'Welcome to this chatroom')
-    while True:
-        try:
-            message = client_socket.recv(2048)
-            if message:
-                print(f'<{client_address[0]}>', message)
-                message_to_send = f'<{client_address[0]}> {message}'
-                message_to_send = bytes(message_to_send, 'utf-8')
-                broadcast(message_to_send, client_socket)
-            else:
-                remove(client_socket)
-        except Exception as ex:
-            continue
 
+# Attemp to Broadcast a Client Message
 def broadcast(message: bytes, client_socket: "socket.socket") -> None:
     for client in LIST_OF_CLIENTS:
         if client != client_socket:
             try:
-                client.send(message)
+                client.sendall(message)
             except Exception as ex:
                 client.close()
-                remove(client)
+                remove_client(client)
 
-def remove(client_socket: "socket.socket") -> None:
-    if client_socket in LIST_OF_CLIENTS:
-        LIST_OF_CLIENTS.remove(client_socket)
 
-print(f'Server started at {IP_ADDRESS}:{PORT} and listening...')
+# Handle Client Method (Clients Secondary Threads)
+def handle_client(client_socket: "socket.socket", client_address: "socket._RetAddress") -> None:
+    try:
+        client_socket.sendall(b'Welcome to this chatroom...')
+        while True:
+            message = client_socket.recv(2048)
+            if not message:
+                remove_client(client_socket)
+                break
+            print(f'<{client_address[0]}>', message.decode('utf-8'))
+            message_to_send = bytes(f'<{client_address[0]}> {message.decode('utf-8')}', 'utf-8')
+            broadcast(message_to_send, client_socket)
+    except Exception as ex:
+        print(f'Error on client {client_address[0]}: {ex}')
+        remove_client(client_socket)
+    finally:
+        client_socket.close()
 
-SERVER_SOCKET.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-SERVER_SOCKET.bind((IP_ADDRESS, PORT))
-SERVER_SOCKET.listen(MAX_CLIENTS)
 
-while True:
-    client_socket, client_address = SERVER_SOCKET.accept()
-    LIST_OF_CLIENTS.append(client_socket)
-    print(client_address[0], 'connected')
-    start_new_thread(client_thread, (client_socket, client_address))
+# Start Server Method (Main Thread)
+def start_server() -> None:
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((IP_ADDRESS, PORT))
+    server_socket.listen(MAX_CLIENTS)
 
-client_socket.close()
-SERVER_SOCKET.close()
+    print(f'Server started at {IP_ADDRESS}:{PORT} and listening...')
+
+    try:
+        while True:
+            client_socket, client_address = server_socket.accept()
+            LIST_OF_CLIENTS.append(client_socket)
+            print(client_address[0], 'connected')
+
+            client_thread = threading.Thread(target = handle_client, args = (client_socket, client_address))
+            client_thread.daemon = True
+            client_thread.start()
+    except Exception as ex:
+        print(f'Error accepting clients: {ex}')
+        print('Closing the server...')
+    finally:
+        for client in LIST_OF_CLIENTS:
+            client.close()
+        server_socket.close()
+
+
+if __name__ == '__main__':
+    start_server()
